477b514f951ef3e6d70de0c5f9dd45f99686f490
austinpaine <austin.paine@ngc.com>
2020-07-20 15:30:33 -0700

    npm install and cljsjs shim files added

--- project.clj
+++ project.clj
@@ -49,70 +49,70 @@
                  [thheller/shadow-cljs "2.10.17" :scope "provided"]]
 
   :min-lein-version "2.0.0"
-  
+
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
   :test-paths ["test/clj"]
   :resource-paths ["resources" "target/cljsbuild"]
   :target-path "target/%s/"
   :main ^:skip-aot vegas.core
 
-  :plugins [[lein-shadow "0.2.0"]] 
+  :plugins [[lein-shadow "0.2.0"]]
   :clean-targets ^{:protect false}
   [:target-path "target/cljsbuild"]
   :shadow-cljs
   {:nrepl {:port 7002}
    :builds
-   {:app
-    {:target :browser
-     :output-dir "target/cljsbuild/public/js"
-     :asset-path "/js"
-     :modules {:app {:entries [vegas.app]}}
-     :devtools
-     {:watch-dir "resources/public" :preloads [re-frisk.preload]}
-     :dev
-     {:closure-defines {"re_frame.trace.trace_enabled_QMARK_" true}}}
-    :test
-    {:target :node-test
-     :output-to "target/test/test.js"
-     :autorun true}}}
-  
+          {:app
+           {:target     :browser
+            :output-dir "target/cljsbuild/public/js"
+            :asset-path "/js"
+            :compiler-options {:output-feature-set :es6}
+            :modules    {:app {:entries [vegas.app]}}
+            :devtools
+                        {:watch-dir "resources/public" :preloads [re-frisk.preload]}
+            :dev
+                        {:closure-defines {"re_frame.trace.trace_enabled_QMARK_" true}}}
+           :test
+           {:target    :node-test
+            :output-to "target/test/test.js"
+            :autorun   true}}}
+
   :npm-deps []
   :npm-dev-deps [[xmlhttprequest "1.8.0"]]
 
   :profiles
-  {:uberjar {:omit-source true
-             :prep-tasks ["compile" ["shadow" "release" "app"]]
-             
-             :aot :all
-             :uberjar-name "vegas.jar"
-             :source-paths ["env/prod/clj"  "env/prod/cljs" ]
-             :resource-paths ["env/prod/resources"]}
+  {:uberjar       {:omit-source    true
+                   :prep-tasks     ["compile" ["shadow" "release" "app"]]
+
+                   :aot            :all
+                   :uberjar-name   "vegas.jar"
+                   :source-paths   ["env/prod/clj" "env/prod/cljs"]
+                   :resource-paths ["env/prod/resources"]}
 
    :dev           [:project/dev :profiles/dev]
    :test          [:project/dev :project/test :profiles/test]
 
-   :project/dev  {:jvm-opts ["-Dconf=dev-config.edn" ]
-                  :dependencies [[binaryage/devtools "1.0.2"]
-                                 [cider/piggieback "0.5.0"]
-                                 [pjstadig/humane-test-output "0.10.0"]
-                                 [prone "2020-01-17"]
-                                 [re-frisk "1.3.4"]
-                                 [ring/ring-devel "1.8.1"]
-                                 [ring/ring-mock "0.4.0"]]
-                  :plugins      [[com.jakemccrary/lein-test-refresh "0.24.1"]
-                                 [jonase/eastwood "0.3.5"]] 
-                  
-                  
-                  :source-paths ["env/dev/clj"  "env/dev/cljs" "test/cljs" ]
-                  :resource-paths ["env/dev/resources"]
-                  :repl-options {:init-ns user
-                                 :timeout 120000}
-                  :injections [(require 'pjstadig.humane-test-output)
-                               (pjstadig.humane-test-output/activate!)]}
-   :project/test {:jvm-opts ["-Dconf=test-config.edn" ]
-                  :resource-paths ["env/test/resources"] 
-                  
-                  
-                  }
-   :profiles/dev {}
+   :project/dev   {:jvm-opts       ["-Dconf=dev-config.edn"]
+                   :dependencies   [[binaryage/devtools "1.0.2"]
+                                    [cider/piggieback "0.5.0"]
+                                    [pjstadig/humane-test-output "0.10.0"]
+                                    [prone "2020-01-17"]
+                                    [re-frisk "1.3.4"]
+                                    [ring/ring-devel "1.8.1"]
+                                    [ring/ring-mock "0.4.0"]]
+                   :plugins        [[com.jakemccrary/lein-test-refresh "0.24.1"]
+                                    [jonase/eastwood "0.3.5"]]
+
+
+                   :source-paths   ["env/dev/clj" "env/dev/cljs" "test/cljs"]
+                   :resource-paths ["env/dev/resources"]
+                   :repl-options   {:init-ns user
+                                    :timeout 120000}
+                   :injections     [(require 'pjstadig.humane-test-output)
+                                    (pjstadig.humane-test-output/activate!)]}
+   :project/test  {:jvm-opts       ["-Dconf=test-config.edn"]
+                   :resource-paths ["env/test/resources"]
+
+                   }
+   :profiles/dev  {}
    :profiles/test {}})
